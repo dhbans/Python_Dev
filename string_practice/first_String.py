3e30fdbee27cb3f16e5e4abba4518b10fbad0312
dbansal <dhruvvvbansal@gmail.com>
2018-09-01 10:57:14 +0530

    Print of string programs

---
+++ string_practice/first_String.py
@@ -0,0 +1,12 @@
+# This first string operation
+
+s1 = 'Welcome to "core python" learning'
+print(s1)
+
+# check the effect of 'r' on special characters
+s2 = "This is check of new \n whether success or not"
+print(s2)
+
+s3 = r"This is with 'r'\n new line should not be there"
+print(s3)
+
