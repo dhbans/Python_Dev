6997cd3d5b04a802c75b1babb2c34783c4a81da9
dbansal <dhruvvvbansal@gmail.com>
2018-09-13 14:44:39 +0530

    this working program

--- Hacker_Rank_Challange/nestedlist.py
+++ Hacker_Rank_Challange/nestedlist.py
@@ -13,25 +13,21 @@ if N >= 2 and N <= 5:
 # hera ziping the two list to make 1 nested list
 for s,n in zip(name_list,score_list):
     list1.append([s,n])
-print(list1)
-
 list1.sort(key=lambda x: x[1])
-print(list1)
 dict1 = {}
 
 if len(list1) == 2:
-    for i in range(0, len(list1)-1):
-        if list1[i][1] == list1[i+1][1]:
-            dict1[list1[i][0]] = list1[i][1]
-            dict1[list1[i+1][0]] = list1[i+1][1]
-        else:
-            dict1[list1[i][0]] = list1[i][1]
+            dict1[list1[1][0]] = list1[1][1]
 
-if len(list1) >2:
-    for i in range(1,len(list1)-2):
-        if list1[i][1] == list1[i+1][1]:
-            dict1[list1[i][0]] = list1[i][1]
-            dict1[list1[i+1][0]] = list1[i+1][1]
+if len(list1) > 2:
+    set_list = []
+    for i in range(0,len(list1)):
+        set_list.append(list1[i][1])
+        score_list = list(set(set_list))
+        score_list.sort()
+    for i in range(0,len(list1)):
+        if list1[i][1] == score_list[1]:
+                dict1[list1[i][0]] = list1[i][1]
 
 list2 = list(dict1)
 list2.sort()
